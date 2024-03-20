61ca8403c6c1fe3825c6a956d0ecc11643b96af0
checkymander <checkymander@protonmail.com>
2024-03-20 21:19:40 +0000

    added init

--- Payload_Type/iris/iris/__init__.py
+++ Payload_Type/iris/iris/__init__.py
@@ -11,7 +11,7 @@ modules = glob.glob(f"{searchPath}")
 invalidate_caches()
 for x in modules:
     if not x.endswith("__init__.py") and x[-3:] == ".py":
-        module = import_module(f"{__name__}.agent_functions." + Path(x).stem)
+        module = import_module(f"{__name__}.mythic.agent_functions." + Path(x).stem)
         for el in dir(module):
             if "__" not in el:
                 globals()[el] = getattr(module, el)
