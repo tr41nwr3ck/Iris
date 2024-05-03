fb765d6b3a673b4a012685fb0a3a5d3d8bcd6383
checkymander <checkymander@protonmail.com>
2024-05-03 14:17:59 -0400

    removed old downloaders

--- Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/GetCallbackByUUIDTool.py
+++ Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/GetCallbackByUUIDTool.py
@@ -19,15 +19,14 @@ class GetCallbackByUUIDTool(BaseTool):
     def validate_environment(cls, values: Dict) -> Dict:
         return values
 
-    def _run(self, agent_callback_id: str):
-        raise Exception("Sync is not supported!")
-        # search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=agent_callback_id)
-        # response = await SendMythicRPCCallbackSearch(search_message)
+    async def _run(self, agent_callback_id: str):
+        search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=agent_callback_id)
+        response = await SendMythicRPCCallbackSearch(search_message)
 
-        # if response.Success:
-        #     return json.dumps(response.Results[0])
-        # else:
-        #     return json.dumps({"message","Callback Not Found"})
+        if response.Success:
+            return json.dumps(response.Results[0])
+        else:
+            return json.dumps({"message","Callback Not Found"})
 
     async def _arun(self, agent_callback_id: str):
         search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=agent_callback_id)
