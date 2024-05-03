3e793ac5aad8b256eee6d8be6128957920ba1b61
checkymander <checkymander@protonmail.com>
2024-05-03 13:42:39 -0400

    added module files

--- Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/GetCallbackByUUIDTool.py
+++ Payload_Type/iris/iris/mythic/agent_functions/helpers/tools/GetCallbackByUUIDTool.py
@@ -1,8 +1,8 @@
 from langchain.tools import BaseTool
-from typing import Self, Type, Union
+from typing import Dict, Self, Type, Union
 from mythic_container.MythicCommandBase import *
 from mythic_container.MythicRPC import *
-from pydantic import BaseModel, Field, ValidationError, model_validator
+from pydantic import BaseModel, Field, ValidationError, root_validator
 import asyncio
 import json
 
@@ -15,9 +15,9 @@ class GetCallbackByUUIDTool(BaseTool):
     description = "use this tool when you need to find a specific callback and know its agent_callback_id (UUID)"
     args_schema: Type[GetCallbackByUUIDSchema] = GetCallbackByUUIDSchema
     
-    @model_validator(mode='before')
-    def check_true(self) -> Self:
-        return self
+    @root_validator(mode='before')
+    def validate_environment(cls, values: Dict) -> Dict:
+        return values
 
     def _run(self, agent_callback_id: str):
         search_message = MythicRPCCallbackSearchMessage(AgentCallbackUUID=agent_callback_id)
